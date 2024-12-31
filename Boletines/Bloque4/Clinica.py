db2f43d3f9ff702eadad25382e5b473e6c61b230
Gabby <gabrieledreira@gmail.com>
2024-12-31 17:06:13 +0100

    Documentacion

--- Boletines/Bloque4/Clinica.py
+++ Boletines/Bloque4/Clinica.py
@@ -6,12 +6,21 @@ class Clinica():
     _Enfermeras_ = dict()
 
     def __init__(self):
+        """Constructor
+        """
         pass
 
     @property
     def get_Medicos(self):
+        """Método get de los médicos
+
+        Returns:
+            list(Trabajador.Medico): Lista de médicos
+        """        
         return self._Medicos_
     def print_Medicos(self):
+        """Imprime solo los médicos de la clínica
+        """        
         aux = self._Medicos_.items()
         print(" -------- Clínica: -------- ")
 
@@ -23,6 +32,14 @@ class Clinica():
         print(" --------------------------\n")
     
     def insert_Medico(self, nuevoMedico:Trabajador.Medico):
+        """Método para la inserción de una medico en una clínica
+
+        Args:
+            nuevoMedico (Trabajador.Medico): Medico a insertar
+
+        Raises:
+            ValueError: Lanza un error si el parametro es núlo, es decir, tiene un formato erroneo
+        """
         if not nuevoMedico:
             raise ValueError("El médico no puede ser inválido.")
         if self._Medicos_.keys().__contains__(nuevoMedico.getNIF()):
@@ -31,6 +48,14 @@ class Clinica():
         self._Medicos_[nuevoMedico.get_NIF()] = nuevoMedico
         
     def delete_Medico(self, NIF_Medico:str):
+        """Método para el borrado de una medico en una clínica
+
+        Args:
+            NIF_Medico (str): NIF de la medico a eliminar
+
+        Raises:
+            ValueError: Lanza un error si los parametros de búsqueda tienen un formato erroneo
+        """
         if not NIF_Medico:
             raise ValueError("El NIF del médico no puede ser vacío.")
         if not self._Medicos_.keys().__contains__(NIF_Medico):
@@ -40,8 +65,15 @@ class Clinica():
 
     @property
     def get_Enfermeras(self):
+        """Método get de Enfermeras
+
+        Returns:
+            list(Trabajador.Enfermera): Lista de enfermeras en la clínica
+        """
         return self._Enfermeras_.items()
     def print_Enfermeras(self):
+        """Imprime por pantalla solo las enfermeras
+        """
         aux = self._Enfermeras__.items()
         print(" -------- Clínica: -------- ")
 
@@ -53,6 +85,14 @@ class Clinica():
         print(" --------------------------\n")
     
     def insert_Enfermera(self, nuevaEnfermera:Trabajador.Enfermera):
+        """Método para la inserción de una enfermera en una clínica
+
+        Args:
+            nuevaEnfermera (Trabajador.Enfermera): Enfermera a insertar
+
+        Raises:
+            ValueError: Lanza un error si el parametro es núlo, es decir, tiene un formato erroneo
+        """
         if not nuevaEnfermera:
             raise ValueError("La enfermera no puede ser inválida.")
         if self._Enfermeras_.keys().__contains__(nuevaEnfermera.getNIF()):
@@ -61,6 +101,14 @@ class Clinica():
         self._Enfermeras_[nuevaEnfermera.get_NIF()] = nuevaEnfermera
         
     def delete_Enfermera(self, NIF_Enfermera:str): 
+        """Método para el borrado de una enfermera en una clínica
+
+        Args:
+            NIF_Enfermera (str): NIF de la enfermera a eliminar
+
+        Raises:
+            ValueError: Lanza un error si los parametros de búsqueda tienen un formato erroneo
+        """
         if not NIF_Enfermera:
             raise ValueError("El NIF de la enfermera no puede ser vacío.")
         if not self._Enfermeras_.keys().__contains__(NIF_Enfermera):
@@ -69,14 +117,27 @@ class Clinica():
         self._Enfermeras_.pop(NIF_Enfermera)
     
     
-    def AñadirTrabajadorAuto(self, NIF, Nombre, FechaNac, Sexo, optional1, optional2, option, NumeroColegiado = None):
-            match(option):
-                case "1":
-                    self._Medicos_[NIF] = Trabajador.Medico(NIF, Nombre, FechaNac, Sexo, optional1, optional2, NumeroColegiado)
+    def AñadirTrabajadorAuto(self, NIF:str, Nombre:str, FechaNac:datetime, Sexo:str, optional1:str, optional2:any, option:str, NumeroColegiado:int = None) -> None:
+        """Método para crear un trabajador de manera directa
+        
+        Args:
+            NIF (str): El NIF de un trabajador
+            Nombre (str): El nombre de un trabajador
+            FechaNacimiento (datetime): La fecha de nacimiento de un trabajador
+            optional1 (str): Puede ser la especialidad de un médico o el area de una enfermera.
+            optional2 (datetime, str): Puede ser la fecha de carrera de un médico o el número de personas de una enfermera
+            option (str): La opción depende de ser Médico, 1, o Enfermera, 2.
+            NumeroColegiado (float, optional): Número de colegiado del trabajador. Defaults to None.
+        """             
+        match(option):
+            case "1":
+                self._Medicos_[NIF] = Trabajador.Medico(NIF, Nombre, FechaNac, Sexo, optional1, optional2, NumeroColegiado)
 
-                case "2":
-                    self._Enfermeras_[NIF] = Trabajador.Enfermera(NIF, Nombre, FechaNac, Sexo, optional1, optional2, NumeroColegiado)                 
-    def AñadirTrabajador(self):
+            case "2":
+                self._Enfermeras_[NIF] = Trabajador.Enfermera(NIF, Nombre, FechaNac, Sexo, optional1, optional2, NumeroColegiado)                 
+    def AñadirTrabajador(self) -> None:
+        """Método para la creación de Trabajadores mediante la inserción de un usuario
+        """        
         aux = None
         while(aux != "Medico" and aux != "Enfermera"):
             aux = input("¿Medico o Enfermera? ")
@@ -94,7 +155,7 @@ class Clinica():
         match(aux):
             case "Medico":
                 Especialidad = input("¿Especialidad? ")
-                FechaCarrera = input("¿Cuando empezó a ejercer? ")
+                FechaCarrera = input("¿Cuando empezó a ejercer (dd/MM/yyyy)? ")
 
                 self._Medicos_[NIF] = Trabajador.Medico(NIF, Nombre, FechaNac, Sexo, Especialidad, FechaCarrera, NumeroColegiado)
 
@@ -105,6 +166,8 @@ class Clinica():
                 self._Enfermeras_[NIF] = Trabajador.Enfermera(NIF, Nombre, FechaNac, Sexo, Area, NumPersonas, NumeroColegiado)
 
     def BorrarTrabajador(self):
+        """Método para el borrado de un trabajador
+        """        
         NIF = input("¿NIF? ")
 
         if NIF in self._Medicos_.keys(): 
@@ -115,7 +178,16 @@ class Clinica():
             print("No existen el Trabajador.\n")
 
 
-    def GetTrabajador(self, option = None) -> Trabajador:
+    def GetTrabajador(self, option:str = None) -> Trabajador:
+        """Método para la obtención de un trabajador, si el atributo "option" está cubierto con una string que sea 
+           "Medico" o "Enfermera" entonces filtrará por esos trabajadores, en cualquier caso se ignorará
+
+        Args:
+            option (str, optional): Filtro entre médicos y enfermeras. Defaults to None.
+
+        Returns:
+            Trabajador: Trabajador encontrado con el NIF coincidente
+        """        
         NIF = input("¿NIF? ")
 
         trabajador = None
@@ -130,6 +202,9 @@ class Clinica():
         return trabajador
     
     def printTrabajadores(self):
+        """Muestra todos los trabajadores de la clínica
+        """        
+        
         trabajadores = list(self._Medicos_.values()) + list(self._Enfermeras_.values())
 
         print(" -------- Clínica: -------- ")
@@ -143,7 +218,12 @@ class Clinica():
         print(" --------------------------\n")
         
         
-    def Inicializar(self, ok = True):
+    def Inicializar(self, ok:bool = True):
+        """Método que crea una serie de trabajadores para una clínica
+
+        Args:
+            ok (bool, optional): Atributo que determina la ejecución o no de la inicialización. Defaults to True.
+        """        
         if (ok):
             self.AñadirTrabajadorAuto('54128586H', 'Gabby', datetime.datetime(2000, 9, 9), 'H', 'Cirugia', datetime.datetime(2022, 9, 9), '1', 1)
             self.AñadirTrabajadorAuto('54128586M', 'Gabby', datetime.datetime(2000, 9, 9), 'M', 'Trato de Personas', 0, '2')
--- Boletines/Bloque4/Trabajador.py
+++ Boletines/Bloque4/Trabajador.py
@@ -1,7 +1,16 @@
 import datetime
 
 class Trabajador:
-    def __init__(self, NIF, Nombre, FechaNacimiento, Sexo, NumeroColegiado = None):
+    def __init__(self, NIF:str, Nombre:str, FechaNacimiento:datetime, Sexo:str, NumeroColegiado:int = None):
+        """Constructor
+
+        Args:
+            NIF (str): NIF del Trabajador
+            Nombre (str): Nombre del Trabajador
+            FechaNacimiento (datetime): Fecha de Nacimiento del Trabajador
+            Sexo (str): Sexo del Trabajador
+            NumeroColegiado (int, optional): Numero de colegiado del Trabajador. Defaults to None.
+        """        
         self._NIF = NIF
         self._Nombre = Nombre
         self._FechaNacimiento = FechaNacimiento
@@ -37,9 +46,22 @@ class Trabajador:
 
     @property
     def get_NumeroColegiado(self):
+        """Método get para NumeroColegiado
+
+        Returns:
+            int: Número de Colegiado de un Trabajador
+        """        
         return self._NumeroColegiado
     @property
     def set_NumeroColegiado(self, nuevo_NumeroColegiado):
+        """Método set de NumeroColegiado
+
+        Args:
+            nuevo_NumeroColegiado (int): Número de colegiado de un trabajador
+
+        Raises:
+            ValueError: Lanza error si el valor a settear es nulo
+        """        
         if not nuevo_NumeroColegiado:
             raise ValueError("El número de colegiado no puede estar vacío.")
         self._NumeroColegiado = nuevo_NumeroColegiado
@@ -47,11 +69,18 @@ class Trabajador:
 
 class Medico(Trabajador):
     def __init__(self, NIF:str, Nombre:str, FechaNacimiento:datetime, Sexo:str, Especialidad:str, FechaInicioCarrera:datetime, NumeroColegiado:float = None):
+        """Constructor
+        """           
         super().__init__(NIF, Nombre, FechaNacimiento, Sexo, NumeroColegiado)
         self._Especialidad = Especialidad
         self._FechaInicioCarrera = FechaInicioCarrera
 
     def getNumeroAnhos(self):
+        """Método para la obtención del número de años que lleva ejerciciendo un médico
+
+        Returns:
+            int: Número de años ejerciciendo
+        """        
         current_date = datetime.datetime.now()
         date = self._FechaInicioCarrera
         
@@ -61,6 +90,11 @@ class Medico(Trabajador):
         return dif
     
     def __str__(self):
+        """Método str sobreescrito
+
+        Returns:
+            str: Una cadena con una estructura en concreto
+        """   
         colegiado = self._NumeroColegiado if self._NumeroColegiado else ""
         return (f"Médico: {self._Nombre} [{self._NIF}], Sexo: {self._Sexo}, " +
                 f"Fecha de nacimiento: {self._FechaNacimiento.strftime('%d/%m/%Y')}, " +
@@ -71,17 +105,23 @@ class Medico(Trabajador):
 class Enfermera(Trabajador):
     _PersonasCargo = 0
     def __init__(self, NIF:str, Nombre:str, FechaNacimiento:datetime, Sexo:str, AreaTrabajo:str, PersonasCargo:int, NumeroColegiado:float = None):
+        """Constructor
+        """        
         super().__init__(NIF, Nombre, FechaNacimiento, Sexo, NumeroColegiado)
         self._AreaTrabajo = AreaTrabajo
         self._PersonasCargo = PersonasCargo
 
     def getPersonasCargo(self):
+        """Método para la obtención del parámetro PersonasCargo de una enfermera
+
+        Returns:
+            int: Número de personas a cargo de la enfermera
+        """
         return self._PersonasCargo
 
-    """
-        Falta Modificarlo para empezar a gestionar personas realmente
-    """
     def AñadirPersonaCargo(self):
+        """Este método podría gestionar personas en si pero por ahora no
+        """        
         # NIF = input("¿NIF? ")
         # Nombre = input("¿Nombre? ")
 
@@ -89,10 +129,9 @@ class Enfermera(Trabajador):
         self._PersonasCargo += 1
         print(f"Se ha añadido una persona. {self._PersonasCargo}\n")
 
-    """
-        Falta Modificarlo para empezar a gestionar personas realmente
-    """
     def BorrarPersonaCargo(self):
+        """Este método podría gestionar personas en si pero por ahora no
+        """
         # NIF = input("¿NIF? ")
 
         # if (self._PersonasCargo_.keys().__contains__(NIF)):
@@ -101,6 +140,11 @@ class Enfermera(Trabajador):
         print(f"Se ha eliminado una persona. {self._PersonasCargo}\n")
 
     def __str__(self):
+        """Método str sobreescrito
+
+        Returns:
+            str: Una cadena con una estructura en concreto
+        """        
         colegiado = self._NumeroColegiado if self._NumeroColegiado else ""
         return (f"Enfermera: {self._Nombre} [{self._NIF}], Sexo: {self._Sexo}, " +
                 f"Fecha de nacimiento: {self._FechaNacimiento.strftime('%d/%m/%Y')}, " +
